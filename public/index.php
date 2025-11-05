e2b01ac657a69166642a670cd3d5f92a9d3d1a48
XoDeR <xoder.vs@gmail.com>
2025-11-05 19:11:13 +0100

    disabled deprecated error reporting

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -53,3 +55,5 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
+
+
