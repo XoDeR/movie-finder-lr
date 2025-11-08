36b75a03e7a932f18a4e355e84b929f0138a8852
XoDeR <xoder.vs@gmail.com>
2025-11-08 15:37:50 +0100

    movie migration

--- database/migrations/2025_11_08_141423_create_movies_table.php
+++ database/migrations/2025_11_08_141423_create_movies_table.php
@@ -4,15 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('movies', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id')->primary();
+            $table->string('imdb_id')->nullable();
+            $table->bigInteger('yts_id')->nullable();
+            $table->string('image')->nullable();
+            $table->string('name');
+            $table->json('genres')->nullable();
+            $table->integer('my_rating')->nullable();
+            $table->year('released_date')->nullable()->index();
+            $table->date('downloaded_status')->nullable()->index();
+            $table->date('watched_status')->nullable()->index();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
